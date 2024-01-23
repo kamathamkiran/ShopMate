a47146b121549c9b614d78cd286b8368d603dee2
K KIRAN <111232471+kamathamkiran@users.noreply.github.com>
2024-01-23 23:08:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ const appSettings = {
     databaseURL: "https://shopping-5db3a-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
+const Username = localStorage.getItem("username");
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingList = ref(database, 'Shopping List')
+const shoppingList = ref(database, `${Username}`)
 
 const CartButton = document.getElementById('add-btn')
 const inputEl = document.getElementById('input-field')
 const listEl = document.getElementById('shopping-list')
+const loginBtn = document.getElementById("login-btn")
 
 CartButton.addEventListener('click', () => {
     let inputValue = inputEl.value
@@ -50,7 +52,26 @@ function addToCart(item) {
     listEl.append(newEl)
 
     newEl.addEventListener("click", () => {
-        let locationOfItem = ref(database, `Shopping List/${itemId}`)
+        let locationOfItem = ref(database, `${Username}/${itemId}`)
         remove(locationOfItem)
     })
 }
+
+loginBtn.addEventListener("click",()=>{
+    let username = document.getElementById("username").value;
+    console.log("Logged in as: " + username);
+    localStorage.setItem("isLoggedIn", "true");
+    localStorage.setItem("username", username);
+    document.getElementById("login-overlay").style.display = "none";
+    document.getElementById("content").style.display = "block";
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    let isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (!isLoggedIn) {
+        document.getElementById("login-overlay").style.display = "flex";
+    } 
+    else {
+        document.getElementById("content").style.display = "block";
+    }
+})
